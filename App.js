187c3d57c48376b1742c77d37e9584ea0d551539
Absolom Mugwagwa <absmugz09@gmail.com>
2017-12-11 15:06:18 +0000

    build languages list

--- App.js
+++ App.js
@@ -1,22 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Content, List, ListItem, Text } from 'native-base';
+
 
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
+
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Text>Hello, Matric Maths App!</Text>
-        <Button
-          onPress={() => navigate('Lessons')}
-          title="IsiNdebele"
-        />
-      </View>
+       <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Matric Maths</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Right>
+        </Header>
+        
+         <Content>
+          <List>
+            <ListItem onPress={() => navigate('Lessons')}>
+              <Text>English</Text>
+            </ListItem>
+            <ListItem onPress={() => navigate('Lessons')}>
+              <Text>Ndebele</Text>
+            </ListItem>
+            <ListItem onPress={() => navigate('Lessons')}>
+              <Text>Afrikaans</Text>
+            </ListItem>
+             <ListItem>
+              <Text>Sotho : Coming Soon</Text>
+            </ListItem>
+             <ListItem>
+              <Text>Tsonga : Coming Soon</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Tswana : Coming Soon</Text>
+            </ListItem>
+             <ListItem>
+              <Text>Venda : Coming Soon</Text>
+            </ListItem>
+             <ListItem>
+              <Text>Xhosa : Coming Soon</Text>
+            </ListItem>
+             <ListItem>
+              <Text>Zulu : Coming Soon</Text>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+   
     );
   }
 }
